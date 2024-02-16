6ffe851503058ce604f8c602f9083f025fe04946
Nikhil Sinha <nikhil.sinha1991@gmail.com>
2024-02-16 14:50:09 +0530

    Initialized config files, packages and setup script

---
+++ setup.js
@@ -0,0 +1,44 @@
+// -- Configure env --
+require("dotenv").config()
+
+// -- Declare variables --
+const {
+  TEST_DB_1_PORT,
+  TEST_DB_2_PORT,
+  TEST_DB_ADMIN_USER,
+  TEST_DB_ADMIN_PASSWORD,
+} = process.env
+const testDb1Url = `http://localhost:${TEST_DB_1_PORT}`
+const testDb2Url = `http://localhost:${TEST_DB_2_PORT}`
+
+// -- Helpers --
+const createDb = async ({ baseUrl, name }) => {
+  const response = await fetch(`${baseUrl}/${name}`, {
+    method: "PUT",
+    headers: new Headers({
+      Authorization:
+        "Basic " + btoa(`${TEST_DB_ADMIN_USER}:${TEST_DB_ADMIN_PASSWORD}`),
+    }),
+  })
+  if (!response.ok) {
+    throw new Error(await response.text())
+  }
+}
+
+// -- Main --
+const main = async () => {
+  // -- Create _users dbs --
+  await createDb({ baseUrl: testDb1Url, name: "_users" })
+  await createDb({ baseUrl: testDb2Url, name: "_users" })
+  // -- Return --
+  return true
+}
+
+// -- Run --
+main()
+  .then((data) => {
+    console.log("Setup successful")
+  })
+  .catch((err) => {
+    console.error(err)
+  })
